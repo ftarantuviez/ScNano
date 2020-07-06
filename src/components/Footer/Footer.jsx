797ff59aedcd7714555bbca7ef3e8afd5c76e878
Francisco Tarantuviez <ftarantuviezz@gmail.com>
2020-07-05 21:25:19 -0300

    Lets do the api

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import {FooterContainer, Anchor} from './styles'
+
+export const Footer = () =>(
+    <FooterContainer className="fluid-container">
+        <p>Created by <Anchor href="https://franciscot.dev" target="_blank"><strong> Francisco Tarantuviez </strong></Anchor></p>
+    </FooterContainer>
+)
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+export const FooterContainer = styled.div`
+    height: 80px;
+    margin-top: 40px;
+    background: #101010;
+    border-top-left-radius: 13px;
+    border-top-right-radius: 13px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: white;
+`
+
+export const Anchor = styled.a`
+    color: rgb(228, 177, 74);
+`
