580b48107baf94a2d2b1c646fb98d65bf2d51469
Francisco Tarantuviez <ftarantuviezz@gmail.com>
2020-07-06 20:49:34 -0300

    FavoritesPage

---
+++ src/components/FavoritesPage/FavoritesPage.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class FavoritesPage extends Component{
+    render(){
+        return(
+            <h1>asdhf</h1>
+        )
+
+    }
+}
+
+export default FavoritesPage;
